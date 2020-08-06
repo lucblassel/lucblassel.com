296931888cf14255179bdef42d68162cd477f957
Luc Blassel <luc.blassel@gmail.com>
2020-08-06 18:44:34 +0200

    adding social icons

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 import { getSortedPostData } from "../lib/posts";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBriefcase, faGraduationCap } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub, faOrcid} from "@fortawesome/free-brands-svg-icons"
 
 export default function Home({ allPostsData }) {
   return (
@@ -42,6 +45,27 @@ export default function Home({ allPostsData }) {
               </p>
             </a>
           </Link>
+
+          <div className={styles.socialCard}>
+            <h3>Find me on the internet</h3>
+            <div className={styles.iconContainer}>
+              <a href="https://github.com/lucblassel">
+                <FontAwesomeIcon icon={ faGithub } height="2rem"/>
+              </a>
+              <a href="https://research.pasteur.fr/en/member/luc-blassel/">
+              <FontAwesomeIcon icon={ faBriefcase } height="2rem"/>
+              </a>
+              <a href="https://www.linkedin.com/in/luc-blassel/">
+              <FontAwesomeIcon icon={ faLinkedin } height="2rem"/>
+              </a>
+              <a href="https://orcid.org/0000-0002-6598-7673">
+              <FontAwesomeIcon icon={ faOrcid } height="2rem"/>
+              </a>
+              <a href="https://scholar.google.fr/citations?user=RYs6rFwAAAAJ&hl=en&oi=ao">
+              <FontAwesomeIcon icon={ faGraduationCap } height="2rem"/>
+              </a>
+            </div>
+          </div>
         </div>
       </main>
 
