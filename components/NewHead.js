5e27dbb3562639e5127db2deffb78c5ddbe6d5a4
Luc Blassel <luc.blassel@gmail.com>
2020-08-17 18:02:00 +0200

    better favicon

--- components/NewHead.js
+++ components/NewHead.js
@@ -6,7 +6,8 @@ export default function NewHead({ children }) {
             <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"/>
             <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
             <meta name="HandheldFriendly" content="true"/>
-            <link rel="icon" href="icon.svg" type="image/svg" size="any"/>
+            <link rel="icon" href="icon.svg" type="image/svg+xml"/>
+            <link rel="alternate icon" href="favicon.ico"/>
             { children }
         </Head>
     )
